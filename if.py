0701e6a7961cba4150830b6ca1f24d47a1d7f728
Kevin Duhamel Hayes <kevinduhamelh@gmail.com>
2023-03-29 03:05:18 -0300

    practicas

---
+++ if.py
@@ -0,0 +1,17 @@
+# Description: Ejemplo de if
+# edad= int(input("Escribe tu edad: "))
+
+# if edad >= 18:
+#     print("Eres mayor de edad")
+# elif edad == 17:
+#     print("Casi eres mayor de edad")
+# elif edad >= 60:
+#     print("Eres un adulto mayor y tienes descuento")
+# else:
+#     print("Eres menor de edad")
+# print("Fin del programa")
+
+
+edad= int(input("Escribe tu edad: "))
+mensaje= "Eres mayor de edad" if edad >= 18 else "Eres menor de edad"
+print(mensaje)
---
+++ trys.py
@@ -0,0 +1,29 @@
+types
+#boolean
+#num
+#str
+#list
+#tuple
+#dict
+#set
+#falsy
+print (bool(0))
+
+
+number1 = input("Enter a number: ")
+if: number1 = int(number1)
+    print("Number1 is an integer")
+else:
+    print("Number1 is not an integer ")
+signo= input("Enter a sign: ")
+if:
+    signo= [+,-,*,/]
+    print("Signo is a valid string")
+else:
+    print("Signo is not a valid string ")
+number2= input("Enter another number: ")
+if:
+    number2= int(number2)
+    print("Number2 is an integer")
+else:
+    print("Number2 is not an integer ")
